e36f50be6fc57b70b2636f5400876e190eb42568
Janis Kikans <janis.kikans@gmail.com>
2019-03-01 17:10:55 +0200

    added sats_client.py

---
+++ sats_client.py
@@ -0,0 +1,5 @@
+# SATS main client program. 
+import datetime
+
+print("SATS Main Client By Janis Kikans", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), sep=" | ")
+print("--------------------------------")
