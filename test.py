5d47309719715843d33e9fa2391fc9c7b438e99d
Janis Kikans <janis.kikans@gmail.com>
2019-03-01 11:25:43 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+
+
+hello_world_string = "Hello World Again!"
+
+print(hello_world_string)
+print("First occurance of letter _o_: ", hello_world_string.find("o"))
+print("String length: ", len(hello_world_string))
