8f76a81c30b3115e0753a1473ffc328d07668623
Janis Kikans <janis.kikans@gmail.com>
2019-03-03 13:23:12 +0200

    Added client module functions module sats_client_functions.py

--- sats_client.py
+++ sats_client.py
@@ -1,5 +1,20 @@
 # SATS main client program. 
 import datetime
+import sats_client_functions
 
 print("SATS Main Client By Janis Kikans", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), sep=" | ")
-print("--------------------------------\n")
+
+loop = True
+while loop:
+    sats_client_functions.print_menu()
+    choice = input("Enter your choice [1-3]:")
+    choice = int(choice)
+
+    if choice == 1:
+        print("Menu 1 has been selected")
+    elif choice == 2:
+        print("Menu 2 has been selected")
+    elif choice == 3:
+        loop = False
+    else:
+        print("Wrong option selection. Enter any key to try again..")
---
+++ sats_client_functions.py
@@ -0,0 +1,7 @@
+# Main client menu printout
+def print_menu():
+    print(20 * "-", "MENU", 20 * "-")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Exit")
+    print(47 * "-")
