c396498c8a9896bf346f1785b08b28adcf6555aa
Janis Kikans <janis.kikans@gmail.com>
2019-03-14 12:42:12 +0200

    added default opts for arguments dataset, encs

--- sats_encode_faces.py
+++ sats_encode_faces.py
@@ -7,8 +7,8 @@ import os
 
 # Argument parser and parsing command line arguments
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("-i", "--dataset", required = True, help = "Path to dataset directory")
-arg_parser.add_argument("-e", "--encodings", required = True, help = "Path to serialized database of facial encodings")
+arg_parser.add_argument("-i", "--dataset", required = True, default = "dataset", help = "Path to dataset directory")
+arg_parser.add_argument("-e", "--encodings", required = True, default = "encodings.pickle", help = "Path to serialized database of facial encodings")
 arg_parser.add_argument("-d", "--detection-method", type = str, default = "cnn", help = "Desired face detecton model (hog/cnn)")
 args = vars(arg_parser.parse_args())
 
