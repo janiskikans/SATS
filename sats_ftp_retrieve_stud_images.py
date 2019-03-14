a0e485b57489a6c05a7d7250b2aa328cf19e60c7
Janis Kikans <janis.kikans@gmail.com>
2019-03-14 12:13:48 +0200

    functioning image retrieval

--- sats_ftp_retrieve_stud_images.py
+++ sats_ftp_retrieve_stud_images.py
@@ -10,26 +10,46 @@ parser.read('./config/dev_settings_local.ini')
 ftp_address = parser.get('ftp', 'ftp_address')
 ftp_account = parser.get('ftp', 'ftp_account')
 ftp_password = parser.get('ftp', 'ftp_password')
+retr_images = 0
 
 print("[INFO] FTP connection details:", "Address: ", ftp_address, "| Account:", ftp_account, "| Password:", ftp_password)
 
 # Connetion to ftp server
-def ftp_connect(img_ref_list):
+def ftp_connect(img_ref_list, student_sel):
     while True:
         try:
             with ftplib.FTP(ftp_address) as ftp:
                 ftp.login(ftp_account, ftp_password)
-                print(ftp.getwelcome())
-                print("[FTP] Current directory", ftp.pwd())
+                #print(ftp.getwelcome())
+                #print("[FTP] Current directory", ftp.pwd())
                 ftp.dir()
-                ftp_get_images(ftp, img_ref_list)
+                ftp_get_images(ftp, img_ref_list, student_sel)
                 break
         except ftplib.all_errors as e:
             print("[ERROR] Error connecting to FTP server!", e)
 
-def ftp_get_images(ftp, img_ref_list):
-    ftp.cwd("/bildes/studentu_bildes/")
-    print("[FTP] Current path:", ftp.pwd())
+def ftp_get_images(ftp, img_ref_list, student_sel):
+    global retr_images
+
+    ftp.cwd("/bildes/studentu_bildes/%s" %str(student_sel))
+    #print("[FTP] Current path:", ftp.pwd())
+
+    os.chdir('retr_files')
+
+    if not os.path.exists(student_sel):
+        os.mkdir(student_sel)
+        print("[FTP] Directory ", student_sel, " created")
+    else:
+        print("[FTP] Directory ", student_sel, " already exists")
+
+    os.chdir("%s" %str(student_sel)) # Changes to created student's image path
+
+    for image in img_ref_list:
+        ftp.retrbinary("RETR " + str(image[0]), open(str(image[0]), 'wb').write)
+        print("[FTP] Downloaded " + str(image[0]) + " successfully! Image of student with id: " + student_sel)
+        retr_images += 1
+
+    os.chdir("../..") # Returns to main directory after downloading student images
 
 # MySQL connection details
 mydb = mysql.connector.connect(
@@ -55,10 +75,14 @@ for records in student_id_list:
     print("[MYSQL]", str(records[0]))
 print('[MYSQL] Print finished')
 
+print("-" * 30)
+print("[INFO] Getting student images!")
+
+retr_students = 0 # Counts how many student id images downloaded
 # For every student id get image refs and download images
 for student_ids in student_id_list:
     # Get all image references connected to selected student ID
-    print("[MYSQL] Current student ID:", str(student_ids[0]))
+    print("\n[MYSQL] Current student ID:", str(student_ids[0]))
     sql_query = """SELECT pb.ref_link FROM studenti s JOIN bildes_savienojumi bs on s.apliecibas_numurs = bs.apliecibas_numurs JOIN profila_bildes pb on pb.bildes_id = bs.bildes_id WHERE s.apliecibas_numurs='%s'""" % (student_ids[0],)
     mycursor.execute(sql_query)
     myresult = mycursor.fetchall()
@@ -68,7 +92,7 @@ for student_ids in student_id_list:
     if mycursor.rowcount != 0:
         print('[MYSQL] Record count:', mycursor.rowcount)
         print('[MYSQL] Printing values...')
-        print('[MYSQL] student_id_list values:')
+        print('[MYSQL] img_ref_list values:')
 
         for records in img_ref_list:
             print("[MYSQL]", str(records[0]))
@@ -77,6 +101,11 @@ for student_ids in student_id_list:
     else:
         print('[MYSQL] No images found for selected student id!')
 
-    print("[MYSQL] Success! Image refrences retrieved")
+    print("[MYSQL] Success! Image references retrieved")
 
-    # Retrieve each image from FTP server
+    # Retrieve each image from FTP server from specified student
+    ftp_connect(img_ref_list, str(student_ids[0]))
+
+    retr_students += 1
+
+print("\n[INFO] Script finished! Retrieved images of {} students ({} images total)!.".format(retr_students, retr_images))
