d6f79db04541967d855c18f7b0cefde0befd5cb2
Janis Kikans <janis.kikans@gmail.com>
2019-03-16 14:22:07 +0200

    added recognize students option

--- sats_client_functions.py
+++ sats_client_functions.py
@@ -1,6 +1,7 @@
 # Main client menu printout
 def print_menu():
     print(20 * "-", "MENU", 20 * "-")
-    print("1. sats_rec_model_training.py")
-    print("2. Exit")
+    print("1. Retrieve student images and enocode faces (sats_ftp_retrieve_stud_images.py)")
+    print("2. Recognize students (Camera) (sats_recognize_faces_cam.py)")
+    print("3. Exit")
     print(47 * "-")
