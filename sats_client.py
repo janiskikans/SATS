1522b2642f76b5b60cb92b75a23e03fb0892a933
Janis Kikans <janis.kikans@gmail.com>
2019-03-16 14:21:07 +0200

    added student registration script

--- sats_client.py
+++ sats_client.py
@@ -1,18 +1,25 @@
 # SATS main client program. 
 import datetime
 import sats_client_functions
+from sats_student_registration import recognition_cam
 
-print("SATS Main Client By Janis Kikans", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), sep=" | ")
+print("\nSATS Main Client By Janis Kikans\n", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), sep=" | ")
 
 loop = True
 while loop:
     sats_client_functions.print_menu()
-    choice = input("Enter your choice [1-2]:")
+    choice = input("Enter your choice [1-3]:")
     choice = int(choice)
 
     if choice == 1:
-        print("Running sats_rec_model_training.py")
+        print("Starting script...\n")
     elif choice == 2:
+        print("Starting script...\n")
+        auditorium_input = input("[INPUT] Enter auditorium number:")
+        auditorium_input = str(auditorium_input)
+        recognition_cam(encodings_file = "encodings.pickle", display = 0, detection_method = "hog", auditorium = auditorium_input)
+        break
+    elif choice == 3:
         loop = False
     else:
         print("Wrong option selection. Enter any key to try again..")
