25f3fb5f9601c8bfbf060850ab650385e8e646a4
Janis Kikans <janis.kikans@gmail.com>
2019-04-11 16:56:28 +0300

    add course name output

--- get_student_attendance.py
+++ get_student_attendance.py
@@ -5,8 +5,8 @@ from tabulate import tabulate
 
 # Config parser
 parser = ConfigParser()
-#parser.read('config/dev_settings_local.ini') # local
-parser.read('./config/dev_settings.ini') # remote LAN
+parser.read('config/dev_settings_local.ini') # local
+#parser.read('./config/dev_settings.ini') # remote LAN
 
 # Gets all info from studentu_uzskaite table
 def get_all_attendance_data():
@@ -18,11 +18,12 @@ def get_all_attendance_data():
         database = parser.get('db', 'db_database')
     )
 
-    query = """SELECT u.id_studentu_uzskaite, s.vards, s.uzvards, u.apliecibas_numurs, u.registracijas_laiks, u.telpas_numurs, u.nodarbibas_id, p.pasniedzeja_vards, p.pasniedzeja_uzvards
+    query = """SELECT u.id_studentu_uzskaite, s.vards, s.uzvards, u.apliecibas_numurs, u.registracijas_laiks, u.telpas_numurs, n.kursa_nosaukums, u.nodarbibas_id, p.pasniedzeja_vards, p.pasniedzeja_uzvards
                 FROM studentu_uzskaite AS u 
                 INNER JOIN studenti AS s ON u.apliecibas_numurs = s.apliecibas_numurs
                 INNER JOIN macisanas_saraksts AS m ON u.nodarbibas_id = m.nodarbibas_id
-                INNER JOIN pasniedzeji AS p ON m.pasniedzeja_id = p.pasniedzeja_id"""
+                INNER JOIN pasniedzeji AS p ON m.pasniedzeja_id = p.pasniedzeja_id
+                INNER JOIN nodarbibas AS n ON u.nodarbibas_id = n.nodarbibas_id"""
 
     my_cursor = mydb.cursor()
     my_cursor.execute(query)
@@ -42,11 +43,12 @@ def get_attendance_by_lesson_id(lesson_id):
         database = parser.get('db', 'db_database')
     )
 
-    query = """SELECT u.id_studentu_uzskaite, s.vards, s.uzvards, u.apliecibas_numurs, u.registracijas_laiks, u.telpas_numurs, p.pasniedzeja_vards, p.pasniedzeja_uzvards
+    query = """SELECT u.id_studentu_uzskaite, s.vards, s.uzvards, u.apliecibas_numurs, u.registracijas_laiks, u.telpas_numurs, n.kursa_nosaukums, p.pasniedzeja_vards, p.pasniedzeja_uzvards
                 FROM studentu_uzskaite AS u 
                 INNER JOIN studenti AS s ON u.apliecibas_numurs = s.apliecibas_numurs
                 INNER JOIN macisanas_saraksts AS m ON u.nodarbibas_id = m.nodarbibas_id
                 INNER JOIN pasniedzeji AS p ON m.pasniedzeja_id = p.pasniedzeja_id
+                INNER JOIN nodarbibas AS n ON u.nodarbibas_id = n.nodarbibas_id
                 WHERE u.nodarbibas_id = %s"""
 
     my_cursor = mydb.cursor()
@@ -61,9 +63,9 @@ def get_attendance_by_lesson_id(lesson_id):
 def print_attendance_by_lesson(attendance_info, mycursor, lesson_id):
     print("\n[INFO] Found %s attendance records for selected lesson(%s):" %(mycursor.rowcount, lesson_id))
     table = attendance_info
-    print(tabulate(table, headers = ["ID", "Vārds", "Uzvārds", "Apliecības nr.", "Reg. laiks", "Telpas nr.", "Pasn. vārds", "Pasn. uzvārds"], tablefmt = "psql", stralign = "center"))
+    print(tabulate(table, headers = ["ID", "Vārds", "Uzvārds", "Apliecības nr.", "Reg. laiks", "Telpas nr.", "Kursa nosauk.", "Pasn. vārds", "Pasn. uzvārds"], tablefmt = "psql", stralign = "center"))
 
 def print_attendance_all(attendance_info, mycursor):
     print("\n[INFO] Found %s attendance records:" %(mycursor.rowcount))
     table = attendance_info
-    print(tabulate(table, headers = ["ID", "Vārds", "Uzvārds", "Apliecības nr.", "Reg. laiks", "Telpas nr.", "Pasn. vārds", "Pasn. uzvārds", "Nodarbības ID"], tablefmt = "psql", stralign = "center"))
+    print(tabulate(table, headers = ["ID", "Vārds", "Uzvārds", "Apliecības nr.", "Reg. laiks", "Telpas nr.", "Kursa nosauk.", "Nodarbības ID", "Pasn. vārds", "Pasn. uzvārds"], tablefmt = "psql", stralign = "center"))
