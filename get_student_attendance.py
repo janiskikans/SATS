8444c34b3e06f3327c29406d788d64a0956dfdb3
janiskikans <janis.kikans@gmail.com>
2019-04-11 23:37:37 +0300

    add attendance info by student, chg language to LV

--- get_student_attendance.py
+++ get_student_attendance.py
@@ -5,8 +5,8 @@ from tabulate import tabulate
 
 # Config parser
 parser = ConfigParser()
-parser.read('config/dev_settings_local.ini') # local
-#parser.read('./config/dev_settings.ini') # remote LAN
+#parser.read('config/dev_settings_local.ini') # local
+parser.read('./config/dev_settings.ini') # remote LAN
 
 # Gets all info from studentu_uzskaite table
 def get_all_attendance_data():
@@ -60,12 +60,55 @@ def get_attendance_by_lesson_id(lesson_id):
     
     mydb.close()
 
+def get_all_attendance_of_student(student_id):
+    # MySQL connection
+    mydb = mysql.connector.connect(
+        host = parser.get('db', 'db_host'),
+        user = parser.get('db', 'db_user'),
+        passwd = parser.get('db', 'db_passwd'),
+        database = parser.get('db', 'db_database')
+    )
+
+    query = """SELECT su.id_studentu_uzskaite, su.registracijas_laiks, su.telpas_numurs, n.kursa_nosaukums, su.nodarbibas_id, p.pasniedzeja_vards, pasniedzeja_uzvards
+                FROM studentu_uzskaite AS su
+                INNER JOIN nodarbibas AS n ON n.nodarbibas_id = su.nodarbibas_id
+                INNER JOIN macisanas_saraksts AS ms ON su.nodarbibas_id = ms.nodarbibas_id
+                INNER JOIN pasniedzeji AS p ON ms.pasniedzeja_id = p.pasniedzeja_id
+                WHERE su.apliecibas_numurs = %s
+                ORDER BY su.id_studentu_uzskaite"""
+
+    my_cursor = mydb.cursor()
+    my_cursor.execute(query, (student_id,))
+    attendance_info = my_cursor.fetchall()
+
+    # Get student name and surname
+    query_student_name = """SELECT s.uzvards, s.vards FROM studenti AS s
+                            WHERE s.apliecibas_numurs = %s"""
+    my_cursor_student_name = mydb.cursor()
+    my_cursor_student_name.execute(query_student_name, (student_id,))
+    student_name_info = my_cursor_student_name.fetchall()
+
+    # Result print
+    print_attendance_by_student(attendance_info, student_name_info, my_cursor, student_id)
+
+    mydb.close()
+
 def print_attendance_by_lesson(attendance_info, mycursor, lesson_id):
-    print("\n[INFO] Found %s attendance records for selected lesson(%s):" %(mycursor.rowcount, lesson_id))
+    print("\n[INFO] Tika atrasti %s apmeklējuma ieraksti izvēlētajai nodarbībai (%s):" %(mycursor.rowcount, lesson_id))
     table = attendance_info
     print(tabulate(table, headers = ["ID", "Vārds", "Uzvārds", "Apliecības nr.", "Reg. laiks", "Telpas nr.", "Kursa nosauk.", "Pasn. vārds", "Pasn. uzvārds"], tablefmt = "psql", stralign = "center"))
 
 def print_attendance_all(attendance_info, mycursor):
-    print("\n[INFO] Found %s attendance records:" %(mycursor.rowcount))
+    print("\n[INFO] Tika atrasti %s apmeklējuma ieraksti:" %(mycursor.rowcount))
     table = attendance_info
     print(tabulate(table, headers = ["ID", "Vārds", "Uzvārds", "Apliecības nr.", "Reg. laiks", "Telpas nr.", "Kursa nosauk.", "Nodarbības ID", "Pasn. vārds", "Pasn. uzvārds"], tablefmt = "psql", stralign = "center"))
+
+def print_attendance_by_student(attendance_info, student_name_info, mycursor, student_id):
+    for row in student_name_info:
+        print("\n[INFO] Tika atrasti {0} apmeklējuma ieraksti izvēlētajam studentam(ID: {1}, Vārds: {2}, {3})".format(mycursor.rowcount, student_id, row[0], row[1]))
+    
+    if mycursor.rowcount == 0:
+        print("\n[INFO] Netika atrasti apmeklējuma ieraksti!")
+    else:
+        table = attendance_info
+        print(tabulate(table, headers = ["ID", "Reg. laiks", "Telpas nr.", "Kursa nosauk.", "Nodarbības ID", "Pasn. vārds", "Pasn. uzvārds"], tablefmt = "psql", stralign = "center"))
--- sats_client.py
+++ sats_client.py
@@ -2,7 +2,7 @@
 import datetime
 import sats_client_functions
 from sats_student_registration import recognition_cam
-from get_student_attendance import get_all_attendance_data, get_attendance_by_lesson_id
+from get_student_attendance import get_all_attendance_data, get_attendance_by_lesson_id, get_all_attendance_of_student
 from sats_ftp_retrieve_stud_images import ftp_retrieve_stud_images_main
 import sats_manual_attendance # Enter attendance data manually.
 import sats_auditorium_lesson_info # Information about what lessons are happening in selected auditorium today.
@@ -13,41 +13,38 @@ print("\nSATS Main Client By Janis Kikans", datetime.datetime.now().strftime('%Y
 loop = True
 while loop:
     sats_client_functions.print_menu()
-    choice = input("Enter your choice [1-7]:")
+    choice = input("Ievadiet Jūsu izvēli [1-8]:")
     choice = int(choice)
 
     if choice == 1:
-        print("[INFO] Starting script..\n")
         ftp_retrieve_stud_images_main()
     elif choice == 2:
         # Jāievieto pārbaude par to vai ir pieejams "encodings.pickle" fails. Ja nē, tad izmet uz galveno loop ar error message.
 
-        print("[INFO] Starting script..\n")
-        auditorium_input = input("[INPUT] Enter auditorium number:")
+        #auditorium_input = input("[INPUT] Enter auditorium number:")
+        auditorium_input = input("[IEVADE] Ievadiet telpas numuru:")
         auditorium_input = str(auditorium_input)
         
         while True:
             try:
-                webcam_input = int(input("\n[INPUT] Select input webcam (In-built webcam - 0; External webcam - 1):"))
+                webcam_input = int(input("\n[IEVADE] Izvēlieties ieejas kameru (Iebūvētā kamera - 0; Ārējā kamera - 1):"))
                 break
             except:
-                print("[ERROR] Invalid input")
+                print("[KĻŪDA] Nepareiza ievade")
                 
             if (webcam_input == 0) or (webcam_input == 1):
                 continue
             else:
-                print("[ERROR] Invalid webcam selection!")
+                print("[KĻŪDA] Nepareiza kameras izvēle!")
 
         recognition_cam(encodings_file = "encodings.pickle", display = 0, detection_method = "hog", auditorium = auditorium_input, webcam_select = webcam_input)
+        
     elif choice == 3:
-        print("[INFO] Starting script..\n")
         sats_manual_attendance.manual_attendance()
     elif choice == 4:
-        print("[INFO] Starting script..\n")
         get_all_attendance_data()
     elif choice == 5:
-        print("[INFO] Starting script..\n")
-        lesson_id_input = str(input("\n[INPUT] Enter lesson ID:"))
+        lesson_id_input = str(input("\n[IEVADE] Ievadiet nodarbības ID:"))
         get_attendance_by_lesson_id(lesson_id_input)
         """ try:
             lesson_id_input = str(input("\n[INPUT] Enter lesson ID:"))
@@ -55,13 +52,16 @@ while loop:
         except:
             print("[ERROR] There was a problem retrieving attendance data. Try again.") """
     elif choice == 6:
-        print("\n[INFO] Starting script..")
-        auditorium_input = input("\n[INPUT] Enter auditorium number:")
+        student_id_input = input("\n[IEVADE] Ievadiet studenta ID:")
+        student_id_input = str(student_id_input)
+        get_all_attendance_of_student(student_id_input)
+    elif choice == 7:
+        auditorium_input = input("\n[IEVADE] Ievadiet telpas numuru:")
         auditorium_input = str(auditorium_input)
 
         sats_auditorium_lesson_info.get_auditorium_lesson_list(auditorium_input)
-    elif choice == 7:
-        print("[INFO] Exitting SATS..")
+    elif choice == 8:
+        print("[INFO] Iziet no SATS..")
         loop = False
     else:
-        print("[ERROR] Invalid option selection. Enter any key to try again..")
+        print("[KĻŪDA] Nepareiza izvēle. Mēģiniet vēlreiz!")
